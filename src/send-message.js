59f50bffa11e4a146aae8cd7a9853f8fb7a2e6bb
Ashwath A N <ashwath.an@juegostudio.net>
2017-06-09 18:49:12 +0530

    Removed reference comments

--- src/send-message.js
+++ src/send-message.js
@@ -38,56 +38,9 @@ function sendMessage(otherUserId, message, messageType, fileKey)
     } else{
       console.log("Channel Does not exist");
       // Creating channel if channel does not exist;
+      
     }
   });
 }
 
 module.exports = sendMessage;
-
-  // sendMessage( otherUserId, message, messageType?: string, fileKey?: any)
-  // {
-  //   if(!messageType){
-  //     messageType = 'text';
-  //   }
-  //   var myUserId = this.user.uid;
-  //   return new Promise((resolve, reject) => {
-  //     var subscriber = this.db.list('/users',{ query:{ orderByChild: 'uid', equalTo: myUserId } })
-  //     .subscribe( res => { 
-  //       var channelId = this.getChannelIdForUser(otherUserId, res[0].channelList);
-  //       subscriber.unsubscribe();
-  //       var lastActivity = firebase.database.ServerValue.TIMESTAMP;
-  //       if(channelId)
-  //       {
-  //         // Channel exist with other user, push ur message to this channel
-  //         var time = Date.now() + this.serverTimeOffset;
-  //         var messageObj;
-  //         messageObj = {
-  //           uid: this.user.uid,
-  //           userName: this.user.displayName,
-  //           displayPhoto: this.user.displayPhoto,
-  //           message: message,
-  //           messageType: messageType,
-  //           timestamp: time,
-  //           negativetimestamp: - time
-  //         }
-  //         if(messageType === 'image'){
-  //             messageObj.fileKey =  fileKey;
-  //         }
-  //         console.log("Sending message to Channel ~~~~~~~> " + channelId);
-  //         firebase.database().ref('/channel/' + channelId + '/lastMessage').set(message);
-  //         firebase.database().ref('/channel/' + channelId + '/lastActivity').set(lastActivity);
-  //         this.db.list('/channel/' + channelId + '/messages/').push(messageObj)
-  //         .then(messageSentResponse=> { 
-  //           resolve(messageObj); 
-  //         })
-  //         .catch(errorMessage=> { reject(errorMessage) });
-  //       } else{
-  //         //Channel does not exist, create a new channel
-  //         console.log(myUserId + " ------> " + otherUserId);
-  //         this.createNewChannel(myUserId, otherUserId)
-  //         .then(res=>this.sendMessage(otherUserId, message, messageType, fileKey).then(res=>resolve(res)).catch(err => reject(err)))
-  //         .catch(err=> reject(err));
-  //       }
-  //     }, err=> reject(err));
-  //   });
-  // }
