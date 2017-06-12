bd7eff6401fbc0855790e17cbb0fb754b729fa56
Namratha <namratha.kulal@juegostudio.net>
2017-06-12 12:02:00 +0530

    method to set typing status

---
+++ src/set-typing-status.js
@@ -0,0 +1,41 @@
+
+var getChannelIdForUser = require('./get-channelid');
+
+function setIsTypingStatus(otherUserId, typingStatus) 
+{
+  console.log("set Typing status");
+  return new Promise((resolve, reject) => {
+    var channelId = getChannelIdForUser(otherUserId, this.user.channelList);
+    var members;
+    var isTyping;
+    if(channelId) 
+    {
+      this.db.ref('/channel/'+channelId).once('value').then((snapshot)=> {
+          snapshot.forEach((childSnapshot) => {
+            if(childSnapshot.getKey() === "members") 
+            {
+              members = childSnapshot.val();
+            }
+          });
+          if(members[0].uid === this.user.uid && members[1].uid === otherUserId) {
+            isTyping = typingStatus;
+          }
+          if(members[0].uid === otherUserId && members[1].uid === this.user.uid) {
+            isTyping = typingStatus;
+          }
+          members.forEach((member, i) => {
+            if(member.uid === this.user.uid) {
+              this.db.ref('/channel/' + channelId + '/members/'+ i +'/typingIndicator').set(isTyping);
+            }
+          });
+      });
+      resolve("Typing status assigned succesfully");
+    }
+    else 
+    {
+      reject("there is no channel established");
+    }
+  });
+}
+
+module.exports = setIsTypingStatus;
