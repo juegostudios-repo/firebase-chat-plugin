b48f2fee5bb3c35fba1bcdf04696c6f88c2c06ae
Namratha <namratha.kulal@juegostudio.net>
2017-06-12 11:57:35 +0530

    created methods for typing indicator

--- src/firebase-init.js
+++ src/firebase-init.js
@@ -1,6 +1,9 @@
 const firebase = require('firebase');
 const initChat = require('./chat-init');
 const sendMessage = require('./send-message');
+const setIsTypingStatus = require('./set-typing-status');
+const getIsTypingStatus = require('./get-typing-status');
+const getRecentChatList = require('./recent-chat-list');
 
 function FirebaseChat(config) {
     
@@ -8,5 +11,8 @@ function FirebaseChat(config) {
     this.db = this.app.database();
     this.initChat = initChat;
     this.sendMessage = sendMessage;
+    this.setIsTypingStatus = setIsTypingStatus;
+    this.getIsTypingStatus = getIsTypingStatus;
+    this.getRecentChatList = getRecentChatList;
 }
 module.exports = FirebaseChat;
