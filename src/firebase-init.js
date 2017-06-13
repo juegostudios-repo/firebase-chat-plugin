ec95c7be602440bf67d974e4d89d1b57a8989375
Ashwath A N <ashwath.an@juegostudio.net>
2017-06-13 15:40:21 +0530

    Send image and getting image methods added

--- src/firebase-init.js
+++ src/firebase-init.js
@@ -6,15 +6,19 @@ const setIsTypingStatus = require('./set-typing-status');
 const getIsTypingStatus = require('./get-typing-status');
 const getRecentChatList = require('./recent-chat-list');
 const onlineStatus = require('./online-status').onlineStatus;
+//const sendImage = require('./send-image');
+const getFile = require('./get-file');
 
 function FirebaseChat(config) {
-    
     this.app = firebase.initializeApp(config);
     this.db = this.app.database();
     this.initChat = initChat;
     this.sendMessage = sendMessage;
     this.receiveMessage = receiveMessage;
-    
+
+    //this.sendImage = sendImage;
+    this.getFile = getFile;
+
     this.setIsTypingStatus = setIsTypingStatus;
     this.getIsTypingStatus = getIsTypingStatus;
     
@@ -22,6 +26,5 @@ function FirebaseChat(config) {
     this.getOnlineStatus = onlineStatus.getOnlineStatus;
     
     this.getRecentChatList = getRecentChatList;
-
 }
 module.exports = FirebaseChat;
