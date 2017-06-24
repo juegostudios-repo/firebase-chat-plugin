fe3ea2c63932a9d3d038294b61e0df56ca4c7e18
Namratha <namratha.kulal@juegostudio.net>
2017-06-24 17:03:35 +0530

    Added new fields- messageReceived and msgId, created new method pushChannelIdToReceiver

--- src/send-message.js
+++ src/send-message.js
@@ -18,9 +18,8 @@ function sendMessage(otherUserId, message, messageType, fileKey)
       var time = Date.now() + this.serverTimeOffset;
       var messageObj;
       messageObj = {
+        msgId: firebase.database.ServerValue.TIMESTAMP,
         uid: this.user.uid,
-        userName: this.user.displayName,
-        displayPhoto: this.user.displayPhoto,
         message: message,
         messageType: messageType,
         timestamp: time,
@@ -29,24 +28,45 @@ function sendMessage(otherUserId, message, messageType, fileKey)
       if(messageType === 'image'){
         messageObj.fileKey =  fileKey;
       }
+      
       this.db.ref('/channel/' + channelId + '/lastMessage').set(messageObj);
       this.db.ref('/channel/' + channelId + '/lastActivity').set(lastActivity);
       this.db.ref('/channel/' + channelId + '/messages/').push(messageObj)
       .then(messageSentResponse=> { 
+        console.log("came here");
+        /** pushing channelId to the receiver */
+        pushChannelIdToReceiver(otherUserId, channelId, this);
+        /** end */
         resolve(messageObj); 
       })
       .catch(errorMessage=> { reject(errorMessage) });
+       
     } else{
       console.log("Channel Does not exist");
       // Creating channel if channel does not exist;
       createNewChannel(this,this.user.uid, otherUserId)
-      .then(res=>this.sendMessage(otherUserId, message, messageType, fileKey)
-        .then(res=>resolve(res)).catch(err => reject(err))
-      )
+      .then(res=> {
+        this.sendMessage(otherUserId, message, messageType, fileKey)
+          .then(res=>resolve(res)).catch(err => reject(err))
+      })
       .catch(err=> reject(err));
       
     }
   });
+}
+
+function pushChannelIdToReceiver(otherUserId, channelId, self)
+{
+  self.db.ref('/users/' + otherUserId).once('value').then(result => {
+    var key;
+    result.forEach(childKey => {
+      key = childKey.getKey()
+    })
+    var channelObj = {
+      channelId: channelId
+    }
+    self.db.ref('/users/' + otherUserId + '/' + key + '/messageReceived/').push(channelObj);
+  })
 }
 
 module.exports = sendMessage;
