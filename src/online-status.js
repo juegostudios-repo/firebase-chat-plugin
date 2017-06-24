146d171ebd05920808f0d357c9eb3a3404be97d7
Namratha <namratha.kulal@juegostudio.net>
2017-06-24 17:08:30 +0530

    Bug fix

--- src/online-status.js
+++ src/online-status.js
@@ -5,58 +5,39 @@ var getChannelIdForUser = require('./get-channelid');
 
 function setOnlineStatus()
 {
-  var channels = this.user.channelList;
-  var index = 0;
-  var lastSeenAt;
-  if(channels.length < 1) return;
-  if(channels.constructor !== Array){
-    channels = Object.keys(channels).map(key=> channels[key]);
-  }
-  
-  channels.forEach((channel)=>{
-    var members = channel.members;
-    members.forEach((member, i) => {
-      if(member.uid === this.user.uid){
-        index = i;    
-      }
-    });
-    lastSeenAt = firebase.database.ServerValue.TIMESTAMP;
-
-    this.db.ref('/channel/' + channel.channelId + '/members/'+ index +'/lastSeenAt').set(lastSeenAt);
-  });
+  var lastSeenAt = true;
+  userRef = this.db.ref('/users/' + this.user.uid + '/' + this.user.$key + '/lastSeenAt');
+  userRef.set(lastSeenAt);
+  userRef.onDisconnect().set(firebase.database.ServerValue.TIMESTAMP);
 }
 
 function getOnlineStatus(otherUserId)
 {
-  return new Observable((observer)=>{    
-    var channelId = getChannelIdForUser(otherUserId, this.user.channelList);
-    if(!channelId){ 
-      observer.error({ success: false, errorMessage:" There is no channel " }) 
-    }
-    else{
-      getOnlineStatusByChannelId(this, channelId).subscribe(res => { observer.next(res) }, err=> observer.error(err));
-    }
-  });
-}
-
-function getOnlineStatusByChannelId(self, channelId)
-{
-  return new Observable((observer)=>{  
-    self.db.ref('/channel/' + channelId)
-    .on('value', res=>{ 
-      
-      res = res.val();
-      var members = res.members;
-      var onlineStatusResponse ={ };
-      
-      members.forEach((member, i)=>{
-        if(member.uid !== self.user.uid)
-        {
-          onlineStatusResponse.isOnline = (new Date().getTime() + self.serverTimeOffset - member.lastSeenAt) < 10000 ? true : false;
-          onlineStatusResponse.uid = member.uid;
-        }
-      });
-      observer.next(onlineStatusResponse);
+  return new Observable((observer)=>{   
+    this.db.ref('/users/' + otherUserId).once('value').then(result => {
+      var key;
+      result.forEach(childKey => {
+        key = childKey.getKey()
+      })
+      if(key)
+      {
+        this.db.ref('/users/' + otherUserId + '/' + key )
+        .on('value', res => {
+          var result = res.val();
+          var lastSeen = result.lastSeenAt;
+          var onlineStatusResponse ={ };
+          if(lastSeen)
+          {
+            onlineStatusResponse.uid = otherUserId;
+            onlineStatusResponse.isOnline = lastSeen
+          }
+          observer.next(onlineStatusResponse);
+        })
+      }
+      else
+      {
+        observer.error({success: false, errMsg: "No user exists"});
+      }
     });
   });
 }
