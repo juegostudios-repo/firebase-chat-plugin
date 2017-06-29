5a1b1d29356cda18a203ba6d2c9040b55df52b51
Namratha <namratha.kulal@juegostudio.net>
2017-06-29 12:02:34 +0530

    Returns ChannelId

--- src/get-channelid.js
+++ src/get-channelid.js
@@ -1,31 +1,24 @@
 function getChannelIdForUser(userId, channelList)
 {
+  
   var channelId;
   if(!channelList)
     return channelId
   if(channelList.constructor !== Array){
-    // var members = channelList.members;
-    // members.forEach(member => {
-    //   if(member.uid === userId && members.length === 2)
-    //   {
-    //     channelId = channelList.channelId;
-    //   }
-    // })
-    // return channelId;
     channelList = Object.keys(channelList).map(key=> {
-      channelList[key]});
-      
+      channelList[key]}); 
   }
   
   channelList.forEach(channel => {
-    channel.members.forEach(member => {
-      if(member.uid === userId && channel.members.length === 2)
-      {
-        channelId = channel.channelId;
-      }
-    });
+    if(channel.member === userId)
+    {
+      channelId = channel.channelId;
+    }
   });
+ 
   return channelId;
 }
 
 module.exports = getChannelIdForUser;
+
+
