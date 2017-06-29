da7dcd96ea6b2af02fb25c54bf524ec6c64b00d7
Namratha <namratha.kulal@juegostudio.net>
2017-06-29 11:55:08 +0530

    OnlineStatus, UpdateProfilePic & ChannelList meths added. RcvMsg meth is merged with getMsgLst

--- src/firebase-init.js
+++ src/firebase-init.js
@@ -1,34 +1,38 @@
 const firebase = require('firebase');
 const initChat = require('./chat-init');
 const sendMessage = require('./send-message');
-const receiveMessage = require('./receive-message');
+const getMessageList = require('./get-message-list');
+const getChannelList = require('./get-channel-list');
+const onlineStatus = require('./online-status').onlineStatus;
 const setIsTypingStatus = require('./set-typing-status');
 const getIsTypingStatus = require('./get-typing-status');
-const getRecentChatList = require('./recent-chat-list');
-const onlineStatus = require('./online-status').onlineStatus;
 const sendImage = require('./send-image');
 const getFile = require('./get-file');
-const loadPrevMsgs = require('./load-previous-msgs');
+const updateProfilePic = require('./update-profpic');
+const listenToUpdatedChannels = require('./listen-messages');
 
 function FirebaseChat(config) {
     this.app = firebase.initializeApp(config);
     this.db = this.app.database();
     this.initChat = initChat;
+    
     this.sendMessage = sendMessage;
-    this.receiveMessage = receiveMessage;
+
+    this.getMessageList = getMessageList;
+
+    this.getChannelList = getChannelList;
+
+    this.setOnlineStatus = onlineStatus.setOnlineStatus;
+    this.getOnlineStatus = onlineStatus.getOnlineStatus;
 
     this.sendImage = sendImage;
     this.getFile = getFile;
 
     this.setIsTypingStatus = setIsTypingStatus;
     this.getIsTypingStatus = getIsTypingStatus;
-    
-    this.setOnlineStatus = onlineStatus.setOnlineStatus;
-    this.getOnlineStatus = onlineStatus.getOnlineStatus;
-    
-    this.getRecentChatList = getRecentChatList;
 
-    this.loadPrevMsgs = loadPrevMsgs;
+    this.listenToUpdatedChannels = listenToUpdatedChannels;
 
+    this.updateProfilePic = updateProfilePic;
 }
 module.exports = FirebaseChat;
