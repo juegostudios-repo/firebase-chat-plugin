2f1801c26adbd72d5e3d4de19a50a2c2b233980e
Namratha <namratha.kulal@juegostudio.net>
2017-06-29 12:06:38 +0530

    Method to listen updated channels

---
+++ src/listen-messages.js
@@ -0,0 +1,34 @@
+
+var Observable =  require('rxjs/Rx').Observable;
+
+function listenToUpdatedChannels()
+{
+  return new Observable((observer) => {
+    var msgRef = this.db.ref('users/' + this.user.uid + '/' + this.user.$key + '/messageReceived/');
+    msgRef.on('value', snapshot => {
+      var channels = [];
+      snapshot.forEach((childSnapShot) => {
+        channels.push(childSnapShot.val().channelId);
+      });
+      if(channels.length > 0)
+      {
+        var result = {};
+        result.message = "Message Received";
+        result.channelList = channels;
+        deleteReadMsgs(msgRef, snapshot);
+        observer.next(result); 
+      }
+    })
+  });
+}
+
+function deleteReadMsgs(msgRef, channels) 
+{
+   var updates = {};
+   channels.forEach((element) => {
+      updates[element.key] = null;
+    });
+    msgRef.update(updates);
+}
+
+module.exports = listenToUpdatedChannels;
