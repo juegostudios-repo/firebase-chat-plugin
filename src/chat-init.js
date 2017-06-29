99ac06bf79b3de42547b8ed092fae8a976e46de8
Namratha <namratha.kulal@juegostudio.net>
2017-06-29 11:49:16 +0530

    Updating Profile picture and channelList

--- src/chat-init.js
+++ src/chat-init.js
@@ -1,4 +1,5 @@
 const firebase = require('firebase');
+
 function initChat (userId, displayName, displayPhoto)
 {
   var offsetRef = this.db.ref(".info/serverTimeOffset");
@@ -14,31 +15,34 @@ function initChat (userId, displayName, displayPhoto)
         snapshot.forEach((childSnapshot) => {
           this.user = childSnapshot.val();
           this.user.$key = childSnapshot.getKey();
-          getUserChannels(this).then (res =>{
-            this.user.channelList = res;
-            resolve(this.user);
-          });
+          if(displayPhoto)
+          {
+            this.updateProfilePic(displayPhoto);
+          }
           listenToChannelListUpdate(this) 
-          
+          resolve(this.user);  
         });
       } else{
         lastSeenAt = firebase.database.ServerValue.TIMESTAMP;
         var user={
           uid: userId,
           displayName: displayName ? displayName: "User " + userId,
-          displayPhoto: displayPhoto ? displayPhoto : " ",
           lastSeenAt: lastSeenAt
         };
+        if(!displayPhoto)
+        {
+          user.displayPhoto= " ";
+        }
         userRef.push(user)
         .then((pushResponse) => {
           user.$key = pushResponse.key;
           this.user = user;
-          getUserChannels(this).then (res =>{
-            this.user.channelList = res;
-            resolve(user);
-          });
+          if(displayPhoto)
+          {
+            this.updateProfilePic(displayPhoto);
+          }
           listenToChannelListUpdate(this);
-          
+          resolve(user);
         })
         .catch(err=> reject(err));
       }
@@ -46,47 +50,22 @@ function initChat (userId, displayName, displayPhoto)
     .catch(err=> reject(err));
   });
 }
-function getUserChannels(self)
-{
-  var result = [];
-  return new Promise((resolve, reject) => {
-    self.db.ref('/channel/').orderByChild('members/0/uid').equalTo(self.user.uid).once("value")
-  .then(snapShot => {
-    snapShot.forEach(childSnapshot => {
-      result.push({
-        channelId: childSnapshot.getKey(),       
-        members: childSnapshot.val().members
-      })
-    })
-    self.db.ref('/channel/').orderByChild('members/1/uid').equalTo(self.user.uid).once("value")
-    .then(snapShot => {
-      if(snapShot.val() !== null)
-      {
-        snapShot.forEach(childSnapshot => {
-          result.push({
-            channelId: childSnapshot.getKey(),  
-            members: childSnapshot.val().members
-          })
-        });
-      }
-      resolve(result);
-    });
-  }); 
-  })
-}
-
 
 function listenToChannelListUpdate(self)
 {
-  console.log("listenToChannelListUpdate");
+  
   self.db.ref('/users/'+ self.user.uid + '/' + self.user.$key + '/channelList')
   .on("value", (snap)=> {
-    getUserChannels(self).then(res => {
-      self.user.channelList = res;
-    });
+  
+    self.user.channelList = [];
+    snap.forEach(childSnap => {
+      self.user.channelList.push(childSnap.val());
+    })
   });
-  // setInterval(()=>{self.setOnlineStatus()}, 10000);
-  self.setOnlineStatus();
+  setInterval(()=> 
+  {
+    self.setOnlineStatus();
+  } , 10000);
 }
 
 module.exports = initChat;
