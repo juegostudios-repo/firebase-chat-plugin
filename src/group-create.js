37a8a4945b8801296844f6f37b24f43c0519eb14
Namratha <namratha.kulal@juegostudio.net>
2017-07-21 15:06:55 +0530

    Changed group implementation

--- src/group-create.js
+++ src/group-create.js
@@ -2,186 +2,90 @@ const firebase = require('firebase');
 var getChannelIdForUser = require('./get-channelid');
 
 //creating new group
-function groupCreate(userIds, grpId, grpName, grpProfilePic)
+function groupCreate(grpId, grpName, grpProfilePic)
 {
-  var index = userIds.indexOf(this.user.uid);
-  if(index !== -1){
-    userIds.splice(index, 1);
-  }
-  
   return new Promise((resolve, reject) => {
-    var user = {
-      uid: this.user.uid,
-    }
-    var member_one = {
-      user: user
-    };
-    
-    var key = this.db.ref().push().key;
-    var channelId = key;
-    //adding group info
-    var groupInfo = {
-      groupId: grpId,
-      groupName: grpName ? grpName : "group "+ grpId,
-      groupPic: ' '
-    }
-
-    if(userIds.length)
-    {
-      var members = [member_one.user];
-      userIds.forEach((userId, i) => {
-        getUserDetails(this, userId)
-        .then((member) => {
-          if(member)
-          {
-            var otherMember = member.user;
-            members.push(otherMember);
-            if((userIds.length - 1) === i)
-            {
-              createGroupWithIds(this, userIds, members, channelId, grpId, groupInfo)
-              .then(res => {
-                resolve(res);
-              })
-              .catch(err => reject(err));
-            }
-          }
-          else
-          {
-            reject({success: false, errMsg: "Group creation cancelled. Some users doesn't exists."})
-          }
-        })
-        .catch(err => { 
-          reject(err);
-        });    
+    checkGroupId(this, grpId)
+    .then(res => {
+      var user = {
+        uid: this.user.uid,
+      }
+      var member_one = {
+        user: user
+      };
+      
+      var key = this.db.ref().push().key;
+      var channelId = key;
+      //adding group info
+      var groupInfo = {
+        groupId: grpId,
+        groupName: grpName ? grpName : "group "+ grpId,
+        groupPic: grpProfilePic ? grpProfilePic : ' '
+      }
+      var member = [member_one.user];
+      var channelName = "group";
+      var createdAt = Date.now() + this.serverTimeOffset;
+      addToChannelList(this, this.user.uid, channelId, grpId);
+      createChannelCollection(this, member, channelId, createdAt, channelName, groupInfo)
+      .then(grpDetails => {
+        resolve({status: true, responseMessage: {message: "Channel created", channelId: channelId, grpDetails: grpDetails.grpInfo}});
       })
-    }
-    else
-    {
-      reject({success: false, errMsg: "Atleast one member is must"});
-    }
+      .catch(err => reject(err));
+    })
+    .catch(err => {
+      reject(err);
+    });
+    
   })
 }
 
 //adding new members to the existing group
-function groupAddMember(userIds, grpId)
+function groupAddMember(userId, grpId)
 {
-  var index = userIds.indexOf(this.user.uid);
-  if(index !== -1){
-    userIds.splice(index, 1);
-  }
+  console.log("grp add member");
   return new Promise((resolve, reject) => {
-    if(userIds.length)
+    if(userId === this.user.uid)
     {
-      this.db.ref('users/' + this.user.uid + '/channelList/').orderByChild('groupId').equalTo(grpId)
-      .once('value').then(snapshot => {
-        if(snapshot.val())
-        {
-          var channelId;
-          var members = [];
-          snapshot.forEach((childSnapshot) =>{
-            channelId = childSnapshot.val().channelId;
-          });
-          if(channelId)
-          {
-            userIds.forEach((userId, i) => {
-              getUserDetailsWithNoGrpID(this, userId, grpId)
-                .then((member) => {
-                  if(member)
-                  {
-                    var otherMember = member.user;
-                    members.push(otherMember);
-                    if((userIds.length - 1) === i)
-                    {
-                      saveDataToCollection(this, userIds, members, channelId, grpId)
-                      .then(res => resolve(res))
-                      .catch(err => reject(err));
-                    }
-                    
-                  }
-                  else
-                  {
-                    reject({success: false, errMsg: "Group creation cancelled. Some users doesn't exists."})
-                  }
-                })
-                .catch(err => { 
-                  reject({success: false, errMsg: "Group creation cancelled. " + err.errMsg });
-                });    
-              })
-          
-          }
-          else
-          {
-            reject({succes: false, errMsg: "Channel does not exists."});
-          }
-        }
-        else
-        {
-          reject({succes: false, errMsg: grpId + " Group doesn't exists in your channel"});
-        }
-      })
+      reject({succes: false, errMsg: "UserID " + userId + " already present in the group."});
     }
     else
     {
-      reject({succes: false, errMsg: "Atleast one member is must"});
-    }
-    
-  })
-}
-
-//sending a message to the group
-function groupSendMessage(groupId, message, messageType, fileKey)
-{
-  if(!messageType){
-    messageType = 'text';
-  }
-  return new Promise((resolve, reject) => {
-    var channelType = 'group';
-    var channelId = getChannelIdForUser(groupId, this.user.channelList, channelType);
-    var lastActivity = firebase.database.ServerValue.TIMESTAMP;
-    if(channelId)
-    {
-      var time = Date.now() + this.serverTimeOffset;
-      var messageObj;
-      messageObj = {
-        msgId: time/*firebase.database.ServerValue.TIMESTAMP*/,
-        uid: this.user.uid,
-        message: message,
-        messageType: messageType,
-        timestamp: time,
-        negativetimestamp: - time
-      };
-      if(messageType === 'image'){
-        messageObj.fileKey =  fileKey;
-      }
-      this.db.ref('/channel/' + channelId + '/lastActivity').set(lastActivity);
-      this.db.ref('/channel/' + channelId + '/lastMessage').set(messageObj);
-      this.db.ref('/channel/' + channelId + '/messages/').push(messageObj)
-      .then(messageSentResponse=> { 
-        /** pushing channelId to the receiver */
-        this.db.ref('channel/' + channelId + '/members/').once('value')
-        .then(snapshot => {
-          var members = snapshot.val();
-          members.forEach(member => {
-            if(member.uid !== this.user.uid)
-              pushChannelIdToReceiver(member.uid, channelId, this);
-          })
+      var channelType = 'group';
+      var channelId = getChannelIdForUser(grpId, this.user.channelList, channelType);
+      if(channelId)
+      {
+        console.log("channel ID::", channelId);
+        getUserDetailsWithNoGrpID(this, userId, grpId)
+        .then((data) => {
+          if(data)
+          {
+            var member = [];
+            member.push(data.user);
+            addToChannelList(this, userId, channelId, grpId);
+            var channelRef = this.db.ref('channel/' + channelId + '/members/');
+            channelRef.once('value')
+            .then(snapshot => {
+              var result = snapshot.val();
+              result = result.concat(member);
+              addToExistingChannel(channelRef, result);
+              resolve({status: true, responseMessage: {message: "Member added", channelId: channelId}});
+            })
+          }
         })
-        /** end */
-        resolve(messageObj); 
-      })
-      .catch(errorMessage=> { reject({success: false, errorMessage}) });
-    }
-    else
-    {
-      reject({success: false, errMsg: "Channel does not exists."});
+        .catch((err) => reject(err));
+      }
+      else
+      {
+        reject({succes: false, errMsg: "Channel does not exists."});
+      }
     }
   });
 }
 
-
 //delete a group
 function groupDelete(groupId)
 {
+  console.log("delete group");
   return new Promise((resolve, reject) => {
     var userRef = this.db.ref('users/' + this.user.uid + '/channelList/');
     userRef.orderByChild('groupId').equalTo(groupId).once('value')
@@ -216,6 +120,29 @@ function groupDelete(groupId)
   });
 }
 
+//get group information
+function checkGroupId(self, groupId)
+{
+  console.log("getGroupInfo");
+  return new Promise((resolve, reject) => {
+    self.db.ref('channel/').orderByChild('groupId').equalTo(groupId).once('value')
+    .then((res) => {
+      if(res.val())
+      {
+        reject({success: false, errMsg: "GroupID " + groupId + " already exists."});
+      }
+      else
+      {
+        resolve({success: true, successMsg: "GroupID " + groupId + " is unique."});
+      }
+    })
+  .catch(err => {
+    reject({success: false, errMsg: "Something went wrong. Please try again."});
+  })
+
+  });
+}
+
 function getUserDetailsWithNoGrpID(self, userId, grpId)
 {
   return new Promise((resolve, reject)=>{
@@ -225,10 +152,11 @@ function getUserDetailsWithNoGrpID(self, userId, grpId)
       if(snapshot.val())
       {
         var userChannelRef = self.db.ref('/users/' + userId + '/channelList/').orderByChild('groupId').equalTo(grpId)
-        .once('value').then(res => {
+        .once('value')
+        .then(res => {
           if(res.val())
           {
-            reject({success: false, errMsg: "Group " + grpId + " already present in some userIds."});
+            reject({success: false, errMsg: "UserID " + userId + " already present in the group."});
           }
           else
           {
@@ -236,10 +164,9 @@ function getUserDetailsWithNoGrpID(self, userId, grpId)
               uid: snapshot.val().uid,
             }
             resolve({ user: user });
-          }
-         
-          
-        });
+          } 
+        })
+        .catch(err => reject({success: false, errMsg: "UserID " + userId + " already present in the group."}));
         
       } 
       else
@@ -250,34 +177,6 @@ function getUserDetailsWithNoGrpID(self, userId, grpId)
     err=> {reject({success: false, errMsg: "User " + userId + " doesn't exists."})});
   });
 }
-
-function saveDataToCollection(self, userIds, members, channelId, grpId)
-{
-  return new Promise((resolve, reject) => {
-    if(userIds.length === members.length)
-    {
-      userIds.forEach((userId, i) => {
-        addToChannelList(self, userId, channelId, grpId);
-        if((userIds.length-1) === i)
-        {
-          var channelRef = self.db.ref('channel/' + channelId + '/members/');
-          channelRef.once('value')
-          .then(snapshot => {
-            var result = snapshot.val();
-            result = result.concat(members);
-            addToExistingChannel(channelRef, result);
-            resolve({status: true, responseMessage: {message: "Members added", channelId: channelId}});
-          })
-        }
-      })
-    }
-    else
-    {
-      reject({success: false, errMsg: "Group creation cancelled. Group " + grpId + " already present in some userIds."})
-    }
-  })
-}
-
 
 function getUserDetails(self, userId)
 {
@@ -298,44 +197,42 @@ function getUserDetails(self, userId)
   });
 }
 
-function createGroupWithIds(self, userIds, members, channelId, grpId, groupInfo)
-{
-  return new Promise((resolve, reject) => {
-    if(userIds.length === (members.length - 1))
-    {
-      userIds.forEach((userId, i) => {
-        addToChannelList(self, userId, channelId, grpId);
-        if((userIds.length-1) === i)
-        {
-          var channelName = "group";
-          var createdAt = Date.now() + self.serverTimeOffset;
-          addToChannelList(self, self.user.uid, channelId, grpId);
-          createChannelCollection(self, members, channelId, createdAt, channelName, groupInfo);
-          resolve({status: true, responseMessage: {message: "Channel created", channelId: channelId}});
-        }
-      });
-      
-    }
-    else
-    {
-      reject({success: false, errMsg: "Group creation cancelled. Some users doesn't exists."})
-    }
-  });
-}
-
 function createChannelCollection(self, members, channelId, createdAt, channelName, groupInfo)
 {
-  var channel = {
-    channelName: channelName,
-    members: members,
-    createdAt: createdAt,
-    groupId: groupInfo.groupId,
-    groupName: groupInfo.groupName,
-    groupPic: groupInfo.groupPic,
-    lastActivity: firebase.database.ServerValue.TIMESTAMP
-  };
-  self.db.ref('/channel/' + channelId).set(channel);
- 
+  return new Promise((resolve, reject) => {
+    var channel = {
+      channelName: channelName,
+      members: members,
+      createdAt: createdAt,
+      groupId: groupInfo.groupId,
+      groupName: groupInfo.groupName,
+      groupPic: ' ',
+      lastActivity: firebase.database.ServerValue.TIMESTAMP
+    };
+    self.db.ref('/channel/' + channelId).set(channel)
+    .then(res => {
+      var grpInfo = {
+        groupId: groupInfo.groupId,
+        groupName: groupInfo.groupName,
+        groupPic: ' ',
+        channelType: channelName
+      };
+      if(groupInfo.groupPic !== ' ')
+      {
+        self.updateProfilePic(groupInfo.groupPic, 'group', groupInfo.groupId)
+        .then(res => {
+          grpInfo.groupPic = res.fileKey;
+          resolve({grpInfo});
+        })
+        .catch(err => resolve({grpInfo}));
+      }
+      else
+      {
+        resolve({grpInfo});
+      }
+    })
+    .catch(err => reject({success: false, errMsg: "Channel creation failed. "}));
+  });
 }
 
 function addToChannelList(self, userId, channelId, groupId)
@@ -369,4 +266,4 @@ function deleteChannel(self, channelRef, channelDetail)
     channelRef.update(updates);
 }
 
-exports.grpFeature = { groupCreate, groupAddMember, groupSendMessage, groupDelete};
+exports.grpFeature = { groupCreate, groupAddMember, groupDelete };
